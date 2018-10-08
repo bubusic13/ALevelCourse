4362e0bc83eba97097b77abc7bdd0401da9b8bf9
bubusic <bubusic13@gmail.com>
2018-10-08 21:58:32 +0300

    Converter

---
+++ gui/script.js
@@ -0,0 +1,12 @@
+
+var n = 0;
+var arr = [1,2,3,4,0,0,2,0];
+var sum = 0;
+var arrr = [];
+
+for(i = 0; i < arr.length; i++){
+    if(arr[i] == n){
+        arrr.push(i)}};
+for(i = 0; i <arrr.length; i++){
+    alert(arrr[i])
+};
