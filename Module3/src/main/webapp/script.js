463f768b9f5323a0ac67056dbe8afde8dbbd714f
bubusic <bubusic13@gmail.com>
2018-10-24 21:33:35 +0300

    Module3 Script fix

--- Module3/src/main/webapp/script.js
+++ Module3/src/main/webapp/script.js
@@ -1,42 +1,42 @@
-var questionInput = document.getElementById("question-input");
+var questionInput = document.getElementById('question-input');
 
-var answerInput = document.getElementById("answer-input");
+var answerInput = document.getElementById('answer-input');
 
-var question = document.getElementById("question-btn");
-
-var answer = document.getElementById("answer-btn");
+var question = document.getElementById('question-btn');
 
 
-    question.addEventListener('click', function () {
+question.addEventListener('click', function () {
+
+    if (questionInput != "" && answerInput.style.display == 'none') {
         axios.get('question', {
-            params: {question:questionInput.value}
+            params: {question: questionInput.value}
         })
             .then(function (result) {
                 var answer = result.data.answer;
-                document.getElementById("answer-output").value = answer;
-
+                answerInput.style.display = 'block';
+                answerInput.value = answer;
+            })
+            .catch(function (reason) {
+                reason.status == 500;
+                answerInput.style.display = 'block';
+            })
+    }
+    else if (answerInput != "") {
+        var inputQuestion = questionInput.value;
+        var inputAnswer = answerInput.value;
+        var questionAnswer = {
+            question: inputQuestion,
+            answer: inputAnswer
+        };
+        axios.post('question', questionAnswer)
+            .then(function () {
 
             })
             .catch(function (reason) {
                 console.error(reason);
-            })
-        document.getElementById('header').reset();
+            });
+    }
+});
 
-    });
 
-    answer.addEventListener('click', function () {
-            var inputQuestion = questionInput.value;
-            var inputAnswer = answerInput.value;
-            var questionAnswer = {
-                question: inputQuestion,
-                answer:  inputAnswer
-            };
-            axios.post('question', questionAnswer)
-                .then(function () {
 
-                })
-                .catch(function (reason) {
-                    console.error(reason);
-                });
-        document.getElementById('header').reset();
-    });
